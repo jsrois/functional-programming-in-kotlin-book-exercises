977d27dd654fcc0a694b0bb261b100d2c4aad633
Javi Sánchez <javier.sanchez.rois@gmail.com>
2022-07-24 15:02:36 +0200

    2.5

--- src/main/kotlin/Chapter2.kt
+++ src/main/kotlin/Chapter2.kt
@@ -27,3 +27,6 @@ fun <A, B, C> curry(f: (A, B) -> C): (A) -> (B) -> C =
 
 fun <A, B, C> uncurry(f: (A) -> (B) -> C): (A, B) -> C =
     { a: A, b: B -> f(a)(b) }
+
+fun <A, B, C> compose(f: (B) -> C, g: (A) -> B): (A) -> C =
+    { a: A -> f(g(a)) }
